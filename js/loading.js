913da0e49a2371083001042ce6321344507d2821
Joe Cuenca <142828652+jcuenca93@users.noreply.github.com>
2024-02-27 18:17:52 +0000

    Tue, Feb 27, 2024, 10:17 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -8,13 +8,13 @@ int = setInterval(blurring, 30)
 
 function blurring() {
 
-    load++
+    load = load + 1
 
     if (load > 99){
         clearInterval(int)
     }
     loadText.innerText = `${load}%`
-
     loadText.style.opacity = 1 - load/100
-    bg.style.filter = `blur(${(30-load/100)*30}px)`
+
+    bg.style.filter = `blur(${}px)`
 }
